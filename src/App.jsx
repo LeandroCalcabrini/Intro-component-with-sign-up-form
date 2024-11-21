9ab3981c13ff35d7f42c8626ea3fd37bc1e3cb90
Leandro <leacalca@gmail.com>
2024-11-21 13:08:44 -0300

    modificacion de la funcion de handleSubtmit  para mostrar Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import './App.css'
 
- function App (){
+
+
+function App (){
 
   //Estado para el nuevo usuario
   const [nuevoUsuario,setNuevoUsuario] = useState({
@@ -14,6 +16,7 @@ import './App.css'
   //Estado para la lista de Usuarios
   const [listaUsuarios,setListaUsuarios] = useState([]);
 
+  // Estado para la validacion de los errores
   const [errores,setErrores] = useState({
     nombre:false,
     apellido:false,
@@ -21,37 +24,42 @@ import './App.css'
     email:false,
   });
 
+  //funcion obtener el valor de los input
   const handleChange = (e) => {
     const {name,value} = e.target;
     setNuevoUsuario({...nuevoUsuario,[name]:value});
   };
 
+  //funcion al hacer click en el btn del formulario
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // sacamos las funciones por defecto
 
-    const nuevosErrores = {
+
+
+    const nuevosErrores = { // hacemos un objeto booleano, para verificar que el usuario ingrese texto e imail para poder setear la lista de usuarios
       nombre: nuevoUsuario.nombre === '',
       apellido: nuevoUsuario.apellido === '',
       contraseña: nuevoUsuario.contraseña === '',
       email: nuevoUsuario.email === '' || !nuevoUsuario.email.includes('@'),
     };
 
-    if(Object.values(nuevosErrores).includes(true)){
-      setErrores({...errores, ...nuevosErrores})
-    }else{
+    if(Object.values(nuevosErrores).includes(true)){ // si algun valor de alguna propiedad del objeto nuevoErrores, es = "true"...
+      setErrores({...errores, ...nuevosErrores}) // seteamos el estado errores, copiando el valor de errores con los d enuevoserrores.
+   
+    }else{ // si todo son false, es decir, que hay datos en los imput, y hay un imail valido, se procede a setear la listadeUsuarios
+      setErrores({...errores, ...nuevosErrores});
       setListaUsuarios([...listaUsuarios , nuevoUsuario]);
-      setNuevoUsuario({
+      setNuevoUsuario({ // volvemos en blanco el formulario
         nombre:'',
         apellido:'',
         contraseña:'',
         email:'',
       })
     };
-
   };
 
-  const mensajeError = (campo) =>{
-    if(errores[campo] == true){
+  const mensajeError = (campo) =>{ 
+    if(errores[campo]){
       return(
         <p>
           Por favor, ingrese su {campo}
@@ -62,7 +70,8 @@ import './App.css'
     }
   };
 
-  console.log(errores)
+
+
 
   return(
    <>
